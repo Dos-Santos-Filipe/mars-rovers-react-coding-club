f832649a78ea83d8b62cc8f7cf8b3718d61a113f
Filipe Dos Santos <filipesantos.psic@gmail.com>
2025-02-04 22:20:45 -0300

    [WIP]: Show rover position on grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
+const input = ["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"];
 type size = {
   x: number;
   y: number;
 };
 
-type Rover = {
-  x: number;
-  y: number;
-};
+type PlateauProps = {
+  size: size;
+  position: string[];
+}
 
-const Plateau = ({ size }: { size: size }, { rovers }: { rovers: Rover[] }) => {
+
+const Plateau = ({ size, position } : PlateauProps) => {
   const { x, y } = size;
+  const [roverX, roverY, roverDirection] = position[0].split(" ");
+  const rover = { x: Number(roverX), y: Number(roverY), direction: roverDirection };
   const grid: JSX.Element[] = [];
-
+  console.log(rover);
+  
   for (let i = 0; i < x; i++) {
     const row: JSX.Element[] = [];
     for (let j = 0; j < y; j++) {
@@ -23,7 +28,7 @@ const Plateau = ({ size }: { size: size }, { rovers }: { rovers: Rover[] }) => {
           key={`${i}-${j}`}
           style={{ border: "1px solid red", width: "20px", height: "20px" }}
         >
-          {isHere ? "X" : ""}
+          {isHere ? `${rover.direction}` : ""}
         </div>
       );
     }
@@ -36,14 +41,21 @@ const Plateau = ({ size }: { size: size }, { rovers }: { rovers: Rover[] }) => {
   return <div>{grid}</div>;
 };
 
+// const Rover = (input: string[]) => {
+//   const roverPosition = input[1].split(" ").map(Number);
+//   return {rover.}; 
+// };
+
 function App() {
   const [plateauSize, setPlateauSize] = useState<size>({ x: 5, y: 5 });
-  const [rovers, setRovers] = useState<Rover[]>([{ x: 3, y: 3 }]);
+  const [rovers, setRovers] = useState(["2 2 N"]);
 
+  console.log(rovers);
+  
   return (
     <>
       <h1>Mars Rovers</h1>
-      <Plateau size={plateauSize} rovers={rovers}} />
+      <Plateau size={plateauSize} position={rovers} />
     </>
   );
 }
